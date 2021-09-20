38f9265dd90917f396fe51a5642d0590aa89d750
Ibiral <tnt_djurdjura@hotmail.fr>
2021-09-20 22:38:20 +0200

    feat: ajout de points en bas des images permettant de choisir le slide que l'on veut

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,67 +1,94 @@
-import React, {useState} from 'react'
-import './Slider.css'
-import dataSlider from './dataSlider'
-import SliderBtn from './SliderBtn'
+import React, { useState } from "react";
+import "./Slider.css";
+import dataSlider from "./dataSlider";
+import SliderBtn from "./SliderBtn";
 
 export default function Slider() {
+  const [slideAnimation, setSlideAnimation] = useState({
+    index: 1,
+    inProgress: false,
+  });
 
-    const [slideAnimation, setSlideAnimation] = useState({
-        index: 1,
-        inProgress: false
-    })
+  const nextSlide = () => {
+    if (
+      slideAnimation.index !== dataSlider.length &&
+      !slideAnimation.inProgress
+    ) {
+      setSlideAnimation({ index: slideAnimation.index + 1, inProgress: true });
 
-    const nextSlide = () => {
-        if(slideAnimation.index !== dataSlider.length && !slideAnimation.inProgress) {
-            setSlideAnimation({index: slideAnimation.index + 1,
-            inProgress: true})
+      setTimeout(() => {
+        setSlideAnimation({
+          index: slideAnimation.index + 1,
+          inProgress: false,
+        });
+      }, 500);
+    } else if (
+      slideAnimation.index === dataSlider.length &&
+      !slideAnimation.inProgress
+    ) {
+      setSlideAnimation({ index: 1, inProgress: true });
 
-            setTimeout(() => {
-                setSlideAnimation({index: slideAnimation.index +1, inProgress: false})
-            }, 500)
-        }
-        else if(slideAnimation.index === dataSlider.length && !slideAnimation.inProgress) {
-            setSlideAnimation({index: 1, inProgress: true})
-
-            setTimeout(() => {
-                setSlideAnimation({index: 1, inProgress: false})
-            }, 500)
-        }
+      setTimeout(() => {
+        setSlideAnimation({ index: 1, inProgress: false });
+      }, 500);
     }
+  };
 
-    const prevSlide = () => {
-        if(slideAnimation.index !== 1 && !slideAnimation.inProgress) {
-            setSlideAnimation({index: slideAnimation.index - 1,
-            inProgress: true})
+  const prevSlide = () => {
+    if (slideAnimation.index !== 1 && !slideAnimation.inProgress) {
+      setSlideAnimation({ index: slideAnimation.index - 1, inProgress: true });
 
-            setTimeout(() => {
-                setSlideAnimation({index: slideAnimation.index -1, inProgress: false})
-            }, 500)
-        }
-        else if(slideAnimation.index === 1 && !slideAnimation.inProgress) {
-            setSlideAnimation({index: dataSlider.length, inProgress: true})
+      setTimeout(() => {
+        setSlideAnimation({
+          index: slideAnimation.index - 1,
+          inProgress: false,
+        });
+      }, 500);
+    } else if (slideAnimation.index === 1 && !slideAnimation.inProgress) {
+      setSlideAnimation({ index: dataSlider.length, inProgress: true });
 
-            setTimeout(() => {
-                setSlideAnimation({index: dataSlider.length, inProgress: false})
-            }, 500)
-        }
+      setTimeout(() => {
+        setSlideAnimation({ index: dataSlider.length, inProgress: false });
+      }, 500);
     }
+  };
 
-    return (
-        <div className="container-slider">
-            {dataSlider.map((pic, index) => {
-                return(
-                    <div
-                    key={pic.id}
-                    className={slideAnimation.index === index +1 ?
-                    "slide active-anim" : "slide"}
-                    >
-                        <img src={process.env.PUBLIC_URL + `/imgs/img${index +1 }.jpg`} alt ="" />
+  const animDot = (index) => {
+    setSlideAnimation({ index: index, inProgress: false });
+  };
 
-                    </div>
-                )
-            })}
-            <SliderBtn moveSlide={nextSlide} direction={'next'}/>
-            <SliderBtn moveSlide={prevSlide} direction={'prev'}/>
-        </div>
-    )
+  return (
+    <div className="container-slider">
+      {dataSlider.map((pic, index) => {
+        return (
+          <div
+            key={pic.id}
+            className={
+              slideAnimation.index === index + 1 ? "slide active-anim" : "slide"
+            }
+          >
+            <img
+              src={process.env.PUBLIC_URL + `/imgs/img${index + 1}.jpg`}
+              alt=""
+            />
+          </div>
+        );
+      })}
+      <SliderBtn moveSlide={nextSlide} direction={"next"} />
+      <SliderBtn moveSlide={prevSlide} direction={"prev"} />
+
+      <div className="container-dots">
+        {Array.from({ length: dataSlider.length }).map((item, index) => {
+          return (
+            <button
+              className={
+                slideAnimation.index === index + 1 ? "dot active" : "dot"
+              }
+              onClick={() => animDot(index + 1)}
+            ></button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
